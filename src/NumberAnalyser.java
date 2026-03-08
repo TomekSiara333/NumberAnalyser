868d63ce7f599f179f398bb9eb6c8817b1edc415
Tomaszewskii <tomaszz.siar@gmail.com>
2026-03-08 23:44:31 +0100

    Init

--- src/NumberAnalyser.java
+++ src/NumberAnalyser.java
@@ -6,18 +6,24 @@ public class NumberAnalyser {
     public static void main(String[] args)
     {
 
-        boolean GameRunning = true;
+        float sum=0;
+        float average=0;
+        float largestNumber=0;
+        float smallestNumber=0;
+        int evenNumbersAmount=0;
+        boolean gameRunning = true;
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> numbers = new ArrayList<>();
+        ArrayList<Float> numbers = new ArrayList<>();
+        numbers.clear();
         ArrayList<String> menu = new ArrayList<>(List.of(
                 "Sum",
-                "Avearge",
+                "Average",
                 "Largest number",
                 "Smallest number",
                 "How many even numbers"
         ));
 
-        while (GameRunning){
+        while (gameRunning){
             System.out.println("How many numbers would you like to analyse? : ");
             String NumbersAmount = scanner.next();
             int LoopAmount = Integer.parseInt(NumbersAmount);
@@ -27,14 +33,14 @@ public class NumberAnalyser {
             //Filling in the arraylist of numbers
             for(int i = 0;i < LoopAmount;i++)
             {
-                System.out.println("Input number #"+i);
+                System.out.println("Input number #"+(i+1));
                 String number = scanner.next();
-                numbers.add(i,Integer.parseInt(number));
+                numbers.add(i,Float.parseFloat(number));
             }
             /*
 
             System.out.println("1. Sum ");
-            System.out.println("2. Avearge ");
+            System.out.println("2. Average ");
             System.out.println("3. Largest number ");
             System.out.println("4. Smallest number ");
             System.out.println("5. How many even numbers ");
@@ -47,10 +53,25 @@ public class NumberAnalyser {
                 System.out.println(i+1 + "." + menu.get(i));
 
             }
-            String answer = scanner.nextLine().toLowerCase();
-            Switch (answer)
-            case sum,1{
+            String answer = scanner.next().toLowerCase();
+            switch (answer)
+            {
+                case "sum","1":
+                    for(int i = 0;i < numbers.size();i++)
+                    {
+                        sum = sum + numbers.get(i);
 
+                    }
+                    System.out.println(sum);
+                    break;
+                case "average","2":
+                    for (int i=0;i < numbers.size();i++)
+                    {
+                        sum = sum + numbers.get(i);
+                    }
+
+                    average = sum / numbers.size();
+                    System.out.println(average);
             }
 
 
